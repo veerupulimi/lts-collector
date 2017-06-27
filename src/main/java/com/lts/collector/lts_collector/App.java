6a6c2efa680ae37789b43a77e27bd0c504bddecc
veerupulimi86 <veerupulimi86@gmail.com>
2017-06-28 00:23:54 +0530

    initial project version

---
+++ src/main/java/com/lts/collector/lts_collector/App.java
@@ -0,0 +1,13 @@
+package com.lts.collector.lts_collector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
