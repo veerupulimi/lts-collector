041aa4edcfe8a09e874f30bb8e1e4b298fe789a9
veerupulimi86 <veerupulimi86@gmail.com>
2017-06-28 10:54:14 +0530

    econd commit

--- src/main/java/com/lts/collector/lts_collector/App.java
+++ src/main/java/com/lts/collector/lts_collector/App.java
@@ -9,5 +9,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
     }
 }
